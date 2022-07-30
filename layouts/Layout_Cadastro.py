cf84ce10e090ef7a9940a62ad7d0777c9464c551
carmodurante <carmo.durante@gmail.com>
2022-07-29 22:36:20 -0300

    Atividades

--- layouts/Layout_Cadastro.py
+++ layouts/Layout_Cadastro.py
@@ -2,8 +2,8 @@ import PySimpleGUI as sg
 
 from functions.Functions_Diversos import get_file_types
 from layouts.Layout_Components import get_buttons
-from layouts.Layout_Components import get_table_header
 from layouts.Layout_Components import get_horas_minutos
+from layouts.Layout_Components import get_table_header
 
 
 def layout_cadastro_ferramenta(lista_cadastrado_ferramentas):
