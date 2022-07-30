af563046c822edc199add181b0283b371ed66fb5
carmodurante <carmo.durante@gmail.com>
2022-07-29 21:36:29 -0300

    Atividades

--- Principal.py
+++ Principal.py
@@ -3,7 +3,7 @@ import webbrowser as web
 import PySimpleGUI as sg
 
 import functions.Functions_Cadastro as cadastros
-import functions.Functions_Consulta as consulta
+import functions.Functions_Consulta as consultas
 import functions.Functions_Login as login
 import functions.Functions_Reserva as reservas
 from functions.Functions_Diversos import get_link_url_dev5
@@ -20,8 +20,8 @@ if __name__ == '__main__':
 
             # Define Window
             window = sg.Window('App Central de Ferramentaria AudioVisual',
-                               layout_principal(cadastros.get_cadastro_ferramentas(),
-                                                cadastros.get_cadastro_tecnicos(),
+                               layout_principal(cadastros.get_cadastro_ferramenta(),
+                                                cadastros.get_cadastro_tecnico(),
                                                 reservas.get_cadastro_reservas(),
                                                 usuario_logado),
                                resizable=True, size=(1250, 670))
@@ -35,42 +35,42 @@ if __name__ == '__main__':
                         break
 
                     # Cadastrar
-                    elif event == 'CadastrarFerramenta':
-                        cadastros.cadastrar_ferramenta(values)
+                    elif event == 'CadastrarFerramenta':  # Cadastrar Ferramenta
+                        cadastros.cadastrar_ferramenta(values, sg)
 
                     # Modificar
-
+                    elif event == 'ModificarFerramenta':  # Modificar Ferramenta
+                        a = 1
                     # Reservar
-
 
                     # Filtrar
                     elif event == 'FiltrarFerramenta':  # Filtrar Ferramenta Consulta
-                        consulta.filtrar_ferramentas(window, values)
+                        consultas.filtrar_ferramentas(window, values)
 
                     elif event == 'FiltrarTecnico':  # Filtrar Tecnico Consulta
-                        consulta.filtrar_tecnicos(window, values)
+                        consultas.filtrar_tecnicos(window, values)
 
                     elif event == 'FiltrarReserva':  # Filtrar Reserva Consulta
-                        consulta.filtrar_reservas(window, values)
+                        consultas.filtrar_reservas(window, values)
 
                     # Limpar Tela
                     elif event == 'LimparFerramentaCON':  # Limpar Ferramenta Consulta
-                        consulta.limpar_filtros(window, 'ferramenta_CON')
+                        consultas.limpar_filtros(window, 'ferramenta_CON')
 
                     elif event == 'LimparFerramentaCAD':  # Limpar Ferramenta Cadastro
-                        consulta.limpar_filtros(window, 'ferramenta_CAD')
+                        consultas.limpar_filtros(window, 'ferramenta_CAD')
 
                     elif event == 'LimparTecnicoCON':  # Limpar Tecnico Consulta
-                        consulta.limpar_filtros(window, 'tecnico_CON')
+                        consultas.limpar_filtros(window, 'tecnico_CON')
 
                     elif event == 'LimparTecnicoCAD':  # Limpar Tecnico Cadastro
-                        consulta.limpar_filtros(window, 'tecnico_CAD')
+                        consultas.limpar_filtros(window, 'tecnico_CAD')
 
                     elif event == 'LimparReservaCON':  # Limpar Reserva Consulta
-                        consulta.limpar_filtros(window, 'reserva_CON')
+                        consultas.limpar_filtros(window, 'reserva_CON')
 
                     elif event == 'LimparReservaCAD':  # Limpar Reserva Cadastro
-                        consulta.limpar_filtros(window, 'reserva_CAD')
+                        consultas.limpar_filtros(window, 'reserva_CAD')
 
                     # Baixar CSV
 
--- functions/Functions_Cadastro.py
+++ functions/Functions_Cadastro.py
@@ -1,22 +1,69 @@
-def get_cadastro_ferramentas():
-    lista_cadastrado_ferramentas = [['1001', 'Chave Inglesa', 'John Deere', '110V', '1015-U521', '10', 'Polegadas',
-                                     'Metal', '36', 'True'],
-                                    ['1002', 'Chave de Fenda', 'John Deere', 'N/D', '1015-U522', '40', 'Metros',
-                                     'Aluminio', '24', 'False']]
+import os
+
+from PIL import Image
+
+
+def get_cadastro_ferramenta():
+    lista_cadastrado_ferramentas = []
+    with open("content/data/ferramentas.csv", "r") as ferramentas:
+        for linha in ferramentas:
+            linha_separada = linha.split(";")
+            lista_cadastrado_ferramentas.append(linha_separada)
 
     return lista_cadastrado_ferramentas
 
 
-def get_cadastro_tecnicos():
+def get_cadastro_tecnico():
     lista_cadastrado_tecnicos = [['46794179865', 'Carmo Durante Neto', '16992180889', 'Manhã', 'Hell Fire'],
                                  ['12345678910', 'Jose Carlos', '1699111111', 'Noite', 'Titans']]
 
     return lista_cadastrado_tecnicos
 
 
-def cadastrar_ferramenta(values):
-    lista_ferramenta = [values['fDescricao'].strip()]
+def cadastrar_ferramenta(values, sg):
+    # Pega sequencial de id_ferramenta
+    id_ferramenta = 1000
+
+    for ferramenta in get_cadastro_ferramenta():
+        if int(ferramenta[0]) > id_ferramenta:
+            id_ferramenta = int(ferramenta[0])
+
+    id_ferramenta += 1
+
+    try:
+        with open("content/data/ferramentas.csv", "a") as ferramentas_arquivo:
+            lista_ferramenta = [f'\n{id_ferramenta}',
+                                f';{values["fDescricao"].strip()}',
+                                f';{values["fCodFabricante"].strip()}',
+                                f';{values["fCodFabricante"].strip()}',
+                                f';{values["fFabricante"].strip()}',
+                                f';{values["fVoltagem"].strip()}',
+                                f';{values["fHRMaxReserva"].strip()}',
+                                f';{values["fMinMaxReserva"].strip()}',
+                                f';{values["fTamanho"].strip()}',
+                                f';{values["fUnidade"].strip()}',
+                                f';{values["fTipo"].strip()}',
+                                f';{values["fMaterial"].strip()}']
+
+            ferramentas_arquivo.writelines(lista_ferramenta)
+
+    except:
+        sg.popup("Erro ao salvar ferramenta", title='Error', font=8)
+
+    # Salvar Imagem da Ferramenta se existir
+    if values['fImagem'].strip() != '':
+        salvar_imagem('ferramenta', id_ferramenta, values['fImagem'], sg)
 
     return lista_ferramenta
 
 
+def salvar_imagem(tipo, id_number, filename, sg):
+    try:
+        if os.path.exists(filename):
+            image = Image.open(filename)
+            image.save(fp=f'content/data/{tipo}_{id_number}.jpg', format='JPEG')
+        else:
+            sg.popup("Erro ao salvar caminho da imagem", title='Error', font=8)
+
+    except:
+        sg.popup("Erro ao salvar imagem", title='Error', font=8)
